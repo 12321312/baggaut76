f4e01debee14f6ce08ab721a0584a4ad70bd79c7
12321312 <xshadownightx12@gmail.com>
2019-08-19 16:34:55 +0700

    Update callcode.js

--- cmds/user/callcode.js
+++ cmds/user/callcode.js
@@ -1,27 +1,45 @@
 const Discord = module.require("discord.js");
 const fs = require("fs");
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const options = {
+  uri: `https://nukacrypt.com/`,
+  transform: function (body) {
+    return cheerio.load(body);
+  }
+};
 
 exports.run = async (bot, message, args) => { 
-let alfa = "74293332";
-let brava = "87904708";
-let charli = "39313471";
-    
-let a = message.author;
-    let ambed = new Discord.RichEmbed()
-    .setTitle("Коды запуска ракет:")
-    .setTimestamp()
-    .setFooter("Система запуска.", "https://vignette.wikia.nocookie.net/fallout/images/c/c2/Icon_Vault_76.png/revision/latest?cb=20181217214332&path-prefix=ru")
-    .setColor('#c10020')
-    .setThumbnail("https://vignette.wikia.nocookie.net/fallout/images/b/b2/Mini_nuke.png/revision/latest?cb=20110404234408")
-    .addField("Альфа:", alfa)
-    .addField("Браво:", brava)
-    .addField("Чарли:", charli);
-    message.channel.send({embed:ambed});
+    rp(options)
+    .then(($) => {
+      let code = ($('#nuclearcodess').text());
+      let alfa = code.slice(130, -79);
+      let brava = code.slice(138, -71);
+      let charli = code.slice(146, -63);
+      
+      let a = message.author;
+      let ambed = new Discord.RichEmbed()
+      .setTitle("Коды запуска ракет:")
+      .setTimestamp()
+      .setFooter("Система запуска.", "https://vignette.wikia.nocookie.net/fallout/images/c/c2/Icon_Vault_76.png/revision/latest?cb=20181217214332&path-prefix=ru")
+      .setColor('#c10020')
+      .setThumbnail("https://vignette.wikia.nocookie.net/fallout/images/b/b2/Mini_nuke.png/revision/latest?cb=20110404234408")
+      .addField("Альфа:", alfa)
+      .addField("Браво:", brava)
+      .addField("Чарли:", charli);
 
-};
+      message.channel.send({embed:ambed});
+
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+}; 
+
+
 module.exports.command = {
-    name: 'code',
-    aliases: ["код", "коды", "бункер"],
+    name: 'callcode',
+    aliases: ["тесткод", "кодт", "кооооод"],
     description: "Показывает пинг, чо доебался?",
     usage: "usercommand",
     category: "user",
