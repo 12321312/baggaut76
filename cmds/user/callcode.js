93c7bd6c868bc5812c717d0be7f2d15d7c91c12d
12321312 <xshadownightx12@gmail.com>
2019-08-27 13:22:26 +0700

    Update callcode.js

--- cmds/user/callcode.js
+++ cmds/user/callcode.js
@@ -10,30 +10,35 @@ const options = {
 };
 
 exports.run = async (bot, message, args) => { 
-    rp(options)
-    .then(($) => {
-      let code = ($('#nuclearcodess').text());
-      let alfa = code.slice(130, -78);
-      let brava = code.slice(138, -70);
-      let charli = code.slice(146, -62);
-      
-      let a = message.author;
-      let ambed = new Discord.RichEmbed()
-      .setTitle("Коды запуска ракет:")
-      .setTimestamp()
-      .setFooter("Система запуска.", "https://vignette.wikia.nocookie.net/fallout/images/c/c2/Icon_Vault_76.png/revision/latest?cb=20181217214332&path-prefix=ru")
-      .setColor('#c10020')
-      .setThumbnail("https://vignette.wikia.nocookie.net/fallout/images/b/b2/Mini_nuke.png/revision/latest?cb=20110404234408")
-      .addField("Альфа:", alfa)
-      .addField("Браво:", brava)
-      .addField("Чарли:", charli);
+  rp(options)
+  .then(($) => {
+    let code = $('#nuclearcodess').text();
+    let mcode = code.replace(/\s+/g, '');
 
-      message.channel.send({embed:ambed});
+    let alfa = mcode.slice(31, -40);
+    let brava = mcode.slice(39, -32);
+    let charli = mcode.slice(47, -24);
 
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+    if (charli === "UNSOLVED") charli = "Нет данных.";
+    if (brava === "UNSOLVED") brava = "Нет данных.";
+    if (alfa === "UNSOLVED") alfa = "Нет данных.";
+    
+    let a = message.author;
+    let ambed = new Discord.RichEmbed()
+    .setTitle("Коды запуска ракет:")
+    .setDescription(`https://nukacrypt.com/`)
+    .setTimestamp()
+    .setFooter("Система запуска.", "https://vignette.wikia.nocookie.net/fallout/images/c/c2/Icon_Vault_76.png/revision/latest?cb=20181217214332&path-prefix=ru")
+    .setColor('#c10020')
+    .addField("Альфа:", `**${alfa}**`, true)
+    .addField("Браво:", `**${brava}**`, true)
+    .addField("Чарли:", `**${charli}**`, true);
+
+    message.channel.send({embed:ambed});    
+  })
+  .catch((err) => {
+    console.log(err);
+  })
 }; 
 
 
