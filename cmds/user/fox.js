44f96472c18590c39b001526613f562e5e1a17a6
12321312 <xshadownightx12@gmail.com>
2019-08-30 22:19:37 +0700

    Update fox.js

--- cmds/user/fox.js
+++ cmds/user/fox.js
@@ -2,9 +2,9 @@ const Discord = module.require("discord.js");
 const fs = require("fs");
 
 exports.run = async (bot, message, args) => { 
-
+    let randonciks = Math.floor(Math.random() * 100) + 1 ;
       let ambed = new Discord.RichEmbed()
-      .setImage("https://static.life.ru/posts/2018/04/1106876/528a9de443b6a0d3b24b413b53e39c4f__980x.jpg");
+      .setImage("https://randomfox.ca/images/"+ randonciks +".jpg");
 
       message.channel.send({embed:ambed}); 
 
