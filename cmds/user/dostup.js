689b816840e01c0cb368453444e2fabab3d87ee6
12321312 <xshadownightx12@gmail.com>
2019-09-02 06:02:16 +0700

    1

--- cmds/user/dostup.js
+++ cmds/user/dostup.js
@@ -1,35 +1,21 @@
-const Discord = module.require("discord.js");
+const Discord = require("discord.js");
 const fs = require("fs");
-const rp = require('request-promise');
-const cheerio = require('cheerio');
-const options = {
-  uri: `https://bethesda.net/ru/status`,
-  transform: function (body) {
-    return cheerio.load(body);
-  }
-};
+const superagent = require("superagent");
 
 exports.run = async (bot, message, args) => { 
-    rp(options)
-    .then(($) => {
-      let code = $('#operational-status').text();
-      console.log(code);
-
-      /* let a = message.author;
-      let ambed = new Discord.RichEmbed()
+  let {body} = await superagent
+  .get(`https://api.bethesda.net/status/ext-server-status?product_id=8`);
+  console.log(body.fallout76);    
+      /*let ambed = new Discord.RichEmbed()
       .setTitle("Доступ сервера:")
       .setTimestamp()
       .setFooter("Систем доступ.", "https://vignette.wikia.nocookie.net/fallout/images/c/c2/Icon_Vault_76.png/revision/latest?cb=20181217214332&path-prefix=ru")
       .setColor('#c10020')
       .setThumbnail("https://vignette.wikia.nocookie.net/fallout/images/b/b2/Mini_nuke.png/revision/latest?cb=20110404234408")
-      .addField("Статус:", alfa);
+      .addField("Статус:", body.fallout76);
 
       message.channel.send({embed:ambed}); */
 
-    })
-    .catch((err) => {
-      console.log(err);
-    })
 }; 
 
 
