f46982aa759ae61aae962c5cf9d5bdeedf59b0ee
12321312 <xshadownightx12@gmail.com>
2019-09-02 06:35:48 +0700

    Update dostup.js

--- cmds/user/dostup.js
+++ cmds/user/dostup.js
@@ -7,7 +7,7 @@ exports.run = async (bot, message, args) => {
   .get(`https://api.bethesda.net/status/ext-server-status?product_id=8`);
   let fal = xip.body;
 
-  console.log(fal.fallout76);    
+  console.log(fal.platform.response.fallout76);    
       /*let ambed = new Discord.RichEmbed()
       .setTitle("Доступ сервера:")
       .setTimestamp()
