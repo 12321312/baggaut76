f04d0d40a53e562d7bee5734494b07f462ff4663
12321312 <xshadownightx12@gmail.com>
2019-09-02 06:25:35 +0700

    Update dostup.js

--- cmds/user/dostup.js
+++ cmds/user/dostup.js
@@ -5,7 +5,7 @@ const superagent = require("superagent");
 exports.run = async (bot, message, args) => { 
   let {body} = await superagent
   .get(`https://api.bethesda.net/status/ext-server-status?product_id=8`);
-  console.log(body.txt);    
+  console.log(body.text);    
       /*let ambed = new Discord.RichEmbed()
       .setTitle("Доступ сервера:")
       .setTimestamp()
